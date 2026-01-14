fa5b16bc02b2330aca33bf12e84dbb02625a038b
shreyaraiyani90-ai <shreyaraiyani90@gmail.com>
2026-01-14 10:10:42 +0530

    Add files via upload

---
+++ assigiment.py
@@ -0,0 +1,102 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+print("hello, welcome to python...!!")
+hello, welcome to python...!!
+a=(input("Enter first number:"))
+Enter first number:2
+b=(input("Enter second number:"))
+Enter second number:4
+sum=a+b
+print("total=",sum)
+total= 24
+num=int(input("Enter a number:"))
+Enter a number:4
+if num%2==0:
+    print("even number")
+    else
+    
+SyntaxError: invalid syntax
+if num%2==0:
+    print("even number")
+else:
+    print("odd number")
+
+    
+even number
+year=int(input("enter year:"))
+enter year:2025
+if(year %4==0 and year %100!=0)or(year%400==0):
+    print("leap year")
+    else:
+        
+SyntaxError: invalid syntax
+if(year %4==0 and year %100!=0)or(year%400==0):
+    print("leap year")
+else:
+    print ("not a leap year")
+
+    
+not a leap year
+import math
+print("pi value=",math.pi)
+pi value= 3.141592653589793
+PI=3.14
+r=int(input("enter radius:"))
+enter radius:3
+name=input("enter your name :")
+enter your name :shreya
+age=input("enter your age :")
+enter your age :19
+print("Name",name)
+Name shreya
+print("Age",age)
+Age 19
+num=int(input("enter a number:"))
+enter a number:4
+square=num*num
+print("square",square)
+square 16
+area=pt*r*r
+Traceback (most recent call last):
+  File "<pyshell#32>", line 1, in <module>
+    area=pt*r*r
+NameError: name 'pt' is not defined
+area=PI*r*r
+print("area of circal=",area)
+area of circal= 28.259999999999998
+value=input("enter any value:")
+enter any value:4
+print("data type is:",type(value))
+data type is: <class 'str'>
+import math
+num=int(input("enter a number:"))
+enter a number:7
+print("square root=",math.sqrt(num))
+square root= 2.6457513110645907
+print("factorial=",math.factorial(num))
+factorial= 5040
+print("absolute value=",abs(num))
+absolute value= 7
+>>> a=int(input("enter base:"))
+enter base:5
+>>> b=int(input("enter power:"))
+enter power:7
+>>> result=pow(a,b)
+>>> print("power=",result)
+power= 78125
+>>> num=int(input("enter a number:"))
+enter a number:4
+>>> if num>0:
+...     print(input("positive number"))
+... elif num<0:
+...     print(input("nagetive number"))
+... else
+SyntaxError: expected ':'
+>>> if num>0:
+...     print(input("positive number"))
+... elif num<0:
+...     print(input("nagetive number"))
+... else:
+...     print(input("zero"))
+... 
+...     
