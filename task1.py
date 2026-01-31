71ddd3c224c10e5a36e6040274b65e4f46517fac
shreyaraiyani90-ai <shreyaraiyani90@gmail.com>
2026-01-31 20:23:22 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,101 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+p=float(input("enter principal amount:"))
+enter principal amount:2
+r=float(input("enter rate of interest:"))
+enter rate of interest:6
+t=float(input("enter time(in year:"))
+enter time(in year:8
+si=(p*r*t)/100
+           
+
+print("simple first number:")
+           
+simple first number:
+print("simple first number:"))
+SyntaxError: unmatched ')'
+print("simple insert=
+      
+SyntaxError: unterminated string literal (detected at line 1)
+print("simple insert=",si)
+      
+simple insert= 0.96
+a=int(input("enter first number:"))
+      
+enter first number:4
+b=int(input("enter second number:"))
+      
+enter second number:5
+print("maximum number is:",max(a,b))
+      
+maximum number is: 5
+for i in  range(1,6):
+      print(i)
+      
+      print(i)
+
+      
+1
+1
+2
+2
+3
+3
+4
+4
+5
+5
+
+text=input("enter astring:")
+      
+enter astring:4
+length=len(text)
+      
+print("length of the string=",length)
+      
+length of the string= 1
+print("Welcome to python programing!")
+      
+Welcome to python programing!
+text=input("enter a string:")
+      
+enter a string:2
+print("last charcter is:",text[-1])
+      
+last charcter is: 2
+num=int(input("enter a number:"))
+      
+enter a number:7
+if num>0:
+      print("number is positive")
+      else:
+          
+SyntaxError: invalid syntax
+num=int(input("enter a number:"))
+enter a number:5
+if num>0
+SyntaxError: expected ':'
+if num>0:
+    print("number is positive")
+    elif num>0:
+        
+SyntaxError: invalid syntax
+>>> elif num>0:
+...     
+SyntaxError: invalid syntax
+>>> num=int(input("enter a number: "))
+enter a number: 7
+>>> if num>0:
+...     print("number is positive")
+... elif num<0:
+...     print("number is negative")
+... else:
+...     print("number is zero")
+... 
+...     
+number is positive
+>>> print("number is negative")
+number is negative
+>>> else:
+...     
+SyntaxError: invalid syntax
