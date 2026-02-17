6d72e6a0a6bab21449d85332ef49f714750c275a
shreyaraiyani90-ai <shreyaraiyani90@gmail.com>
2026-02-17 08:44:05 +0530

    Add files via upload

---
+++ assigiment 1.py
@@ -0,0 +1,11 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> print("hello world")
+hello world
+>>> a=input("enter the value:")
+enter the value:45
+>>> b=input("enter the value:")
+enter the value:43
+>>> sum=int(a)+int(b)
+>>> print(sum)
+88
